50fb63af6d053907414a45151f92547b16575848
Víctor <lacuentadevictor@gmail.com>
2023-04-02 09:40:34 +0200

    Actualizar stryker.conf.js
    Stryker config

--- stryker.conf.js
+++ stryker.conf.js
@@ -1,17 +1,14 @@
-module.exports = {
-    mutator: "javascript",
-    packageManager: "npm",
-    reporters: ["clear-text", "progress"],
-    testRunner: "mocha",
-    transpilers: [],
-    testFramework: "mocha",
-    coverageAnalysis: "perTest",
-    mochaOptions:{
-      spec: ['domain/*.spec.js']
-    },
-    mutate: ['domain/*.js',
-      '!domain/*.spec.js',
-      '!domain/*.port.js',
-      '!domain/*.factory.js'],
-    maxConcurrentTestRunners: 2
-}
+module.exports = { 
+     packageManager: "npm",
+     reporters: ["clear-text", "progress"],
+     testRunner: "mocha", 
+     coverageAnalysis: "perTest", 
+     mochaOptions:{ 
+       spec: ['domain/*.spec.js'] 
+     }, 
+     mutate: ['domain/*.js', 
+       '!domain/*.spec.js', 
+       '!domain/*.port.js', 
+       '!domain/*.factory.js'], 
+     maxConcurrentTestRunners: 2 
+ }
